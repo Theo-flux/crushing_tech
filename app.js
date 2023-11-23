00ae2e42d5c418ba19c8dcfdb7c07157e4082be1
Theo <ekunnusitheophilus@gmail.com>
2023-11-23 03:26:52 +0100

    feat: setup guides toggle logic

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const toastDesktopCloseEl = document.getElementById("toastDesktopCloseEl");
 const setupGuideTogglerEl = document.getElementById("setupGuideTogglerEl");
 const setupGuideBottomEl = document.getElementById("setupGuideBottomEl");
 const setupGuideArrowEl = document.getElementById("setupGuideArrowEl");
+const personalizedGuideEls = document.querySelectorAll(".personalized_guides");
 
 const handleCloseToast = () => {
   toastEl.style.display = "none";
@@ -55,15 +56,57 @@ setupGuideTogglerEl.addEventListener("click", () => {
   const setupGuideBtmHeight =
     window.getComputedStyle(setupGuideBottomEl).height;
 
-  if (setupGuideBtmHeight === "200px") {
+  console.log(setupGuideBtmHeight);
+  console.log(setupGuideBottomEl.offsetHeight);
+
+  if (setupGuideBtmHeight === "400px") {
     setupGuideBottomEl.style.height = "0px";
     setupGuideBottomEl.style.visibility = "hidden";
     setupGuideBottomEl.style.opacity = "0";
     setupGuideArrowEl.style.rotate = "0deg";
   } else {
-    setupGuideBottomEl.style.height = "200px";
+    setupGuideBottomEl.style.height = "400px";
     setupGuideBottomEl.style.visibility = "visible";
     setupGuideBottomEl.style.opacity = "1";
     setupGuideArrowEl.style.rotate = "180deg";
   }
+});
+
+personalizedGuideEls.forEach((element, index) => {
+  const targetPEl = element.children[0].children[1];
+  const inputCheck = element.children[0].children[0].children[0].children[0];
+
+  targetPEl.addEventListener("click", () => {
+    const isOtherExpanded = [].indexOf.call(
+      personalizedGuideEls,
+      document.querySelector(".expanded_guide"),
+    );
+    const isThisExpanded = targetPEl.classList.contains("expanded_guide_title");
+
+    if (isThisExpanded) {
+      personalizedGuideEls[index].classList.remove("expanded_guide");
+      personalizedGuideEls[index].children[1].classList.remove(
+        "expanded_guide_details",
+      );
+      targetPEl.classList.remove("expanded_guide_title");
+    }
+    personalizedGuideEls[isOtherExpanded].classList.remove("expanded_guide");
+    personalizedGuideEls[isOtherExpanded].children[1].classList.remove(
+      "expanded_guide_details",
+    );
+    personalizedGuideEls[
+      isOtherExpanded
+    ].children[0].children[1].classList.remove("expanded_guide_title");
+
+    targetPEl.classList.add("expanded_guide_title");
+    element.classList.add("expanded_guide");
+    element.children[1].classList.add("expanded_guide_details");
+  });
+
+  inputCheck.addEventListener("change", (e) => {
+    if (e.target.checked) {
+      const clickEvent = new Event("click");
+      targetPEl.dispatchEvent(clickEvent);
+    }
+  });
 });
